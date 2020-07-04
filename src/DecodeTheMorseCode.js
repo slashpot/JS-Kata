ba86054d91940df3f31d7615f890f7cbe9e3950f
Eric Lin <ttttt123465@gmail.com>
2020-07-04 09:30:55 +0800

    add morse code of all characters

--- src/DecodeTheMorseCode.js
+++ src/DecodeTheMorseCode.js
@@ -1,17 +1,57 @@
 function toChar(code) {
     switch (code) {
-        case "....":
-            return "H";
-        case ".":
-            return "E";
-        case "-.--":
-            return "Y";
-        case ".---":
-            return "J";
-        case "..-":
-            return "U";
+        case ".-":
+            return "A";
+        case "-...":
+            return "B";
+        case "-.-.":
+            return "C";
         case "-..":
             return "D";
+        case ".":
+            return "E";
+        case "..-.":
+            return "F";
+        case "--.":
+            return "G";
+        case "....":
+            return "H";
+        case "..":
+            return "I";
+        case ".---":
+            return "J";
+        case "-.-":
+            return "K";
+        case ".-..":
+            return "L";
+        case "--":
+            return "M";
+        case "-.":
+            return "N";
+        case "---":
+            return "O";
+        case ".--.":
+            return "P";
+        case "--.-":
+            return "Q";
+        case ".-.":
+            return "R";
+        case "...":
+            return "S";
+        case "-":
+            return "T";
+        case "..-":
+            return "U";
+        case "...-":
+            return "V";
+        case ".--":
+            return "W";
+        case "-..-":
+            return "X";
+        case "-.--":
+            return "Y";
+        case "--..":
+            return "Z";
     }
 }
 
@@ -21,11 +61,11 @@ export const decodeMorse = function (morseCode) {
 
     return arr.map(code => {
         if (code === "") {
-            const char = isLeadingSpace ? "": " ";
+            const char = isLeadingSpace ? "" : " ";
             isLeadingSpace = true;
             return char;
         }
         isLeadingSpace = false;
         return toChar(code);
-    }).join('');
+    }).join("");
 };
